bedb1aa66fd6b29343e557a6e4390f8575595cdb
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2024-12-15 15:03:37 -0300

    feat: add Appointment class

---
+++ API/Equoterapia/src/main/java/com/equoterapia/web/entities/Appointment.java
@@ -0,0 +1,36 @@
+package com.equoterapia.web.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Time;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_appoitments")
+
+public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Time time;
+
+    @ManyToOne
+    @JoinColumn(name = "professional_id")
+    private Professional professional;
+
+    @ManyToOne
+    @JoinColumn(name = "pacient_id")
+    private Pacient pacient;
+
+    @ManyToOne
+    @JoinColumn(name = "anamnesis_id")
+    private Anamnesis anamnesis;
+
+}
