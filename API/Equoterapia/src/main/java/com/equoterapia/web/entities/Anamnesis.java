d4eceefde206a7cd1a743d9631326b4cc5d7fced
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2024-12-15 15:03:19 -0300

    feat: add Anamnesis class

---
+++ API/Equoterapia/src/main/java/com/equoterapia/web/entities/Anamnesis.java
@@ -0,0 +1,32 @@
+package com.equoterapia.web.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_anamnesis")
+public class Anamnesis {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String reasonEvaluation;
+    private String currentIllnessHistory;
+    private String pastMedicalHistory;
+    private String familiarHistory;
+    private String observation;
+
+    @OneToMany(mappedBy = "anamnesis")
+    private List<Appointment> appointments = new ArrayList<>();
+
+}
