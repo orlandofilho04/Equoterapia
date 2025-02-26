52bfa570bfafdbf88c6556a7f2e9057f43cdb001
Tulio-Eloi <tulioeloi29@gmail.com>
2025-02-26 19:41:18 -0300

    feat: Resource Professional

---
+++ API/Equoterapia/src/main/java/com/equoterapia/web/resources/ProfessionalResource.java
@@ -0,0 +1,56 @@
+package com.equoterapia.web.resources;
+
+import com.equoterapia.web.entities.Professional;
+import com.equoterapia.web.services.ProfessionalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping(value = "/professionals")
+public class ProfessionalResource {
+
+    @Autowired
+    private ProfessionalService professionalService;
+
+    @GetMapping
+    public ResponseEntity<List<Professional>> findAll() {
+        List<Professional> professionals = professionalService.findAll();
+        return ResponseEntity.ok().body(professionals);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Professional> findById(@PathVariable Long id) {
+        Professional professional = professionalService.findById(id);
+        return ResponseEntity.ok().body(professional);
+    }
+
+    @PostMapping
+    public ResponseEntity<Professional> insert(@RequestBody Professional professional) {
+        professional = professionalService.insert(professional);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(professional.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(professional);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Professional> update(@PathVariable Long id, @RequestBody Professional professional) {
+        professional.setId(id);
+        professional = professionalService.update(professional);
+        return ResponseEntity.ok().body(professional);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        professionalService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+}
