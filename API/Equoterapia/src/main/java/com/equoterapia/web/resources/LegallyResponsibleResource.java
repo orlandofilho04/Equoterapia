2aaa07432aca91200351c221f74cf9ed54222b66
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-03-06 15:05:11 -0300

    altera o endpoint de responsavel legal e adiciona trycatch

--- API/Equoterapia/src/main/java/com/equoterapia/web/resources/LegallyResponsibleResource.java
+++ API/Equoterapia/src/main/java/com/equoterapia/web/resources/LegallyResponsibleResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import com.equoterapia.web.entities.LegallyResponsible;
+import com.equoterapia.web.services.PacientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,23 +15,35 @@ import com.equoterapia.web.services.LegallyResponsibleService;
 
 @RestController
 @CrossOrigin("*")
-@RequestMapping(value = "/ResponsavelLegal")
+@RequestMapping(value = "/legallyResponsible")
 public class LegallyResponsibleResource{
     @Autowired
     private LegallyResponsibleService responsibleService;
+    @Autowired
+    private PacientService pacientService;
+    @Autowired
+    private LegallyResponsibleService legallyResponsibleService;
 
     @GetMapping
     public ResponseEntity<List<LegallyResponsible>> findAll(){
         List<LegallyResponsible> legallyResponsibles = responsibleService.findAll();
         return ResponseEntity.ok().body(legallyResponsibles);
     }
-      @PostMapping
-    public ResponseEntity<LegallyResponsible> insert(@RequestBody LegallyResponsible legallyResponsible){
-        legallyResponsible = responsibleService.insert(legallyResponsible);                    //
+    @PostMapping
+    public ResponseEntity<LegallyResponsible> insert(@RequestBody LegallyResponsible legallyResponsible, @RequestParam List<Long> pacient_ids){
+        try {
+            List<Pacient> associatedPacient = pacientService.findAllById(pacient_ids);
+            legallyResponsible.setPacients(associatedPacient);
+
+            legallyResponsible = legallyResponsibleService.insert(legallyResponsible);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
         URI uri = ServletUriComponentsBuilder
                 .fromCurrentRequest()
-                .path("/{id}").buildAndExpand(legallyResponsible.getId()).
-                toUri(); // configurando URI que será retornada ao criar user
+                .path("/{id}").buildAndExpand(legallyResponsible.getId())
+                .toUri();
         return ResponseEntity.created(uri).body(legallyResponsible);
     }
 
