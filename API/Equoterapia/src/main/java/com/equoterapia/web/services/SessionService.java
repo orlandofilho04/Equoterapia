62f7eac73e418899a39cecc963513a622c7f00a3
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-03-06 15:16:47 -0300

    cria metodo para cadastrar uma nova sessao

--- API/Equoterapia/src/main/java/com/equoterapia/web/services/SessionService.java
+++ API/Equoterapia/src/main/java/com/equoterapia/web/services/SessionService.java
@@ -1,5 +1,8 @@
 package com.equoterapia.web.services;
 
+import com.equoterapia.web.entities.Horse;
+import com.equoterapia.web.entities.Pacient;
+import com.equoterapia.web.entities.Professional;
 import com.equoterapia.web.entities.Session;
 import com.equoterapia.web.exceptions.NotFoundException;
 import com.equoterapia.web.repositories.SessionRepository;
@@ -14,7 +17,13 @@ public class SessionService {
     
     @Autowired
     private SessionRepository sessionRepository;
-    
+    @Autowired
+    private PacientService pacientService;
+    @Autowired
+    private HorseService horseService;
+    @Autowired
+    private ProfessionalService professionalService;
+
     public List<Session> findAll() {
         return sessionRepository.findAll();
     }
@@ -24,14 +33,27 @@ public class SessionService {
             throw new NotFoundException("Sessao não encontrada");
         }
         Optional<Session> optionalSession = sessionRepository.findById(id);
-        return optionalSession.orElseThrow(() -> new RuntimeException("Runtime Error"));
+        return optionalSession.orElseThrow(RuntimeException::new);
     }
     
     public Session insert(Session session) {
         return sessionRepository.save(session);
     }
 
-    // para verificar se a sessão existe antes de atualizar
+    public Session cadastrarSessao(Session session,Long pacient_id, Long horse_id, Long professional_id) {
+        Pacient pacient = pacientService.findById(pacient_id);
+        Horse horse = horseService.findById(horse_id);
+        Professional professional = professionalService.findById(professional_id);
+
+        session.getProfessionals().add(professional);
+        session.setEquine(horse);
+        session.setPacient(pacient);
+
+        return sessionRepository.save(session);
+    }
+
+
+
     public Session update(Session session) {
         if (!sessionRepository.existsById(session.getId())){
             throw new NotFoundException("Sessao não encontrada");
@@ -40,7 +62,6 @@ public class SessionService {
         return sessionRepository.save(session);
     }
 
-    //verifica se a sessão existe antes de deletar
     public void delete(Long id) {
         if (!sessionRepository.existsById(id)){
             throw new NotFoundException("Sessao não encontrada");
