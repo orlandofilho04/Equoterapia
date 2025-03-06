3021ea650ebcb01e39faa8a9c2fbae3cc7b5891c
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-03-06 14:12:08 -0300

    documentando rota de consulta e adicionando rota de update

--- API/Equoterapia/src/main/java/com/equoterapia/web/resources/AppointmentResource.java
+++ API/Equoterapia/src/main/java/com/equoterapia/web/resources/AppointmentResource.java
@@ -2,6 +2,9 @@ package com.equoterapia.web.resources;
 
 import com.equoterapia.web.entities.Appointment;
 import com.equoterapia.web.services.AppointmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,12 +21,23 @@ public class AppointmentResource {
     private AppointmentService appointmentService;
 
     @GetMapping
+    @Operation(description = "Endpoint responsável por retornar todas as consultas")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca por consultas concluída"),
+            @ApiResponse(responseCode = "500", description = "Erro Interno do Servidor")
+    })
     public ResponseEntity<List<Appointment>> findAll(){
         List<Appointment> appointmentList = appointmentService.findAll();
         return ResponseEntity.ok().body(appointmentList);
     }
 
     @GetMapping(value = "/{id}")
+    @Operation(description = "Endpoint responsável por buscar uma consulta por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca por consulta concluída"),
+            @ApiResponse(responseCode = "404", description = "Consulta não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro Interno do Servidor")
+    })
     public ResponseEntity<Appointment> findOne(@PathVariable Long id){
         Appointment appointment = appointmentService.findById(id);
 
@@ -31,6 +45,12 @@ public class AppointmentResource {
     }
 
     @PostMapping
+    @Operation(description = "Endpoint responsável por inserir uma consulta")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Consulta criada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Consulta não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro Interno do Servidor")
+    })
     public ResponseEntity<Appointment> insert(@RequestBody Appointment appointment){
         appointment = appointmentService.insert(appointment);
 
@@ -41,8 +61,31 @@ public class AppointmentResource {
                 .toUri();
         return ResponseEntity.created(uri).body(appointment);
     }
+    @PutMapping
+    @Operation(description = "Endpoint responsável por atualizar uma consulta")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Consulta atualizada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Consulta não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro Interno do Servidor")
+    })
+    public ResponseEntity<Appointment> update(@RequestBody Appointment appointment){
+        appointment = appointmentService.update(appointment);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(appointment.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(appointment);
+    }
 
     @DeleteMapping(value = {"/{id}"})
+    @Operation(description = "Endpoint responsável por deletar uma consulta por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Consulta deletada com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Consulta não encontrada"),
+            @ApiResponse(responseCode = "500", description = "Erro Interno do Servidor")
+    })
     public ResponseEntity<Void> delete(@PathVariable Long id){
         appointmentService.delete(id);
 
