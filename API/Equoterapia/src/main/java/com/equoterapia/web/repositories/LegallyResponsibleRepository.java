66d385f3834620e37417c1fb0fe6b26975115d57
Carlos Henrique Mota Martins <107642033+CarlosHMms@users.noreply.github.com>
2025-03-19 18:24:13 -0300

    quebra de linha

--- API/Equoterapia/src/main/java/com/equoterapia/web/repositories/LegallyResponsibleRepository.java
+++ API/Equoterapia/src/main/java/com/equoterapia/web/repositories/LegallyResponsibleRepository.java
@@ -2,7 +2,6 @@
 package com.equoterapia.web.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.equoterapia.web.entities.LegallyResponsible;
 
 public interface LegallyResponsibleRepository extends JpaRepository<LegallyResponsible, Long>{
