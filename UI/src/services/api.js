29808a2834efa57c1d00f32262393eac451ade3d
João pedro Borges Araujo <116849630+jpborgesaraujo@users.noreply.github.com>
2025-04-28 14:53:00 -0300

    teste

--- UI/src/services/api.js
+++ UI/src/services/api.js
@@ -9,8 +9,15 @@ const api = axios.create({
   }
 });
 
+// Token de teste fixo
+const TEST_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhdXRoLWFwaSIsInN1YiI6IkpQMCIsImV4cCI6MTc0NTg3OTY0MH0.Pw_UuzjqK8nKRUibr0WfrusO5fOLA1ITieaIMED5u40';
+
 api.interceptors.request.use(request => {
   console.log('Enviando requisição:', request);
+  
+  // Adiciona o token de autenticação em todas as requisições
+  request.headers['Authorization'] = `Bearer ${TEST_TOKEN}`;
+  
   return request;
 });
 
